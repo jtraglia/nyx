9caf48ae1e0bc118e08755f3536e4f3be19eb21d
Justin Traglia <jtraglia@pm.me>
2025-05-20 14:49:54 -0500

    Initial commit

---
+++ scripts/extract_teku_summary.py
@@ -0,0 +1,24 @@
+import json
+
+from bs4 import BeautifulSoup
+
+with open("teku/eth-reference-tests/build/reports/tests/referenceTest/index.html") as f:
+    soup = BeautifulSoup(f, "html.parser")
+
+def get_counter(id):
+    box = soup.find("div", {"class": "infoBox", "id": id})
+    return box.find("div", class_="counter").text.strip()
+
+total = int(get_counter("tests"))
+failed = int(get_counter("failures"))
+ignored = int(get_counter("ignored"))
+duration = get_counter("duration")
+
+with open("results/teku.json", "w") as out:
+    json.dump({
+        "total": total,
+        "failed": failed,
+        "ignored": ignored,
+        "passed": total - failed - ignored,
+        "duration": duration
+    }, out, indent=2)
